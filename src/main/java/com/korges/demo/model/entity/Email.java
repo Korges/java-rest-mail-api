a4a5e04b536d2767b10d0d02bedc3479e6ad145b
Korges <korgesik@gmail.com>
2020-07-03 22:40:58 +0200

    Remove @Setter and and @Builder to Email.

--- src/main/java/com/korges/demo/model/entity/Email.java
+++ src/main/java/com/korges/demo/model/entity/Email.java
@@ -1,15 +1,18 @@
 package com.korges.demo.model.entity;
 
+import com.korges.demo.model.enums.EmailStatus;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter @Setter
+@Getter
+@Builder
 @Document
 public class Email {
     @Id
     private String id;
     private String header;
     private String message;
+    private EmailStatus status;
 }
