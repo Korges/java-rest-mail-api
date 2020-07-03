b89664d7d71dd6765effc3e974a72f701c87364e
Korges <korgesik@gmail.com>
2020-07-03 19:40:46 +0200

    Initial Commit.

---
+++ src/main/java/com/korges/demo/JavaRestMailApiApplication.java
@@ -0,0 +1,13 @@
+package com.korges.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestMailApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRestMailApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/korges/demo/JavaRestMailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korges.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestMailApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
