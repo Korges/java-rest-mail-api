3166c6e01438f3d71c8bc7d5017aa6133719cdbc
Korges <korgesik@gmail.com>
2020-07-04 10:32:38 +0200

    Generate some boilerplate code on Email.

--- src/main/java/com/korges/demo/model/entity/Email.java
+++ src/main/java/com/korges/demo/model/entity/Email.java
@@ -1,6 +1,7 @@
 package com.korges.demo.model.entity;
 
 import com.korges.demo.model.enums.EmailStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
@@ -15,4 +16,27 @@ public class Email {
     private String header;
     private String message;
     private EmailStatus status;
+
+    public Email() {
+
+    }
+
+    public Email(String id, String header, String message, EmailStatus status) {
+        this.id = id;
+        this.header = header;
+        this.message = message;
+        this.status = status;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setStatus(EmailStatus status) {
+        this.status = status;
+    }
 }
