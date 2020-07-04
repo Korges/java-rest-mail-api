f371a32168fd6de1dfeb7f8c94f8ffa90fbcc3bd
Korges <korgesik@gmail.com>
2020-07-04 18:41:32 +0200

    Add new method findAllByStatus() in EmailRepository.

--- src/main/java/com/korges/demo/repository/EmailRepository.java
+++ src/main/java/com/korges/demo/repository/EmailRepository.java
@@ -1,10 +1,15 @@
 package com.korges.demo.repository;
 
 import com.korges.demo.model.entity.Email;
+import com.korges.demo.model.enums.EmailStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmailRepository extends MongoRepository<Email, String> {
+
+    List<Email> findAllByStatus(EmailStatus emailStatus);
 
 }
