f28d9e0d45d0a0f5e63c8f4cd8a1d55e06664b6e
Korges <korgesik@gmail.com>
2020-07-04 17:46:28 +0200

    Initial implementation of JavaMailSender.

--- src/main/java/com/korges/demo/service/sender/EmailSenderServiceImpl.java
+++ src/main/java/com/korges/demo/service/sender/EmailSenderServiceImpl.java
@@ -3,14 +3,30 @@ package com.korges.demo.service.sender;
 import com.korges.demo.model.entity.Email;
 import com.korges.demo.model.enums.Error;
 import io.vavr.control.Either;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class EmailSenderServiceImpl implements EmailSenderService {
+
+    private final JavaMailSender emailSender;
 
     // TODO
     @Override
     public Either<Error, Email> send(Email email) {
+        if (email.getRecipients().isEmpty()) return Either.left(Error.NO_RECIPIENTS);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setSubject(email.getSubject());
+        message.setText(email.getText());
+        message.setTo(email.getRecipients().toArray(String[]::new));
+
+        emailSender.send(message);
+
         return Either.right(email);
     }
 }
