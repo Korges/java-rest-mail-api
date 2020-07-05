d6d443b89eaa0c9ac9e56551a8dfe58cc2e2283a
Korges <korgesik@gmail.com>
2020-07-05 10:24:03 +0200

    Add @Setter to Email Document.

--- src/main/java/com/korges/demo/model/entity/Email.java
+++ src/main/java/com/korges/demo/model/entity/Email.java
@@ -1,19 +1,22 @@
 package com.korges.demo.model.entity;
 
 import com.korges.demo.model.enums.EmailStatus;
+import io.vavr.collection.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
 
 @AllArgsConstructor
+@Setter
 @Getter
 @Builder
 @Document
 public class Email {
+
     @Id
     private String id;
     private String subject;
