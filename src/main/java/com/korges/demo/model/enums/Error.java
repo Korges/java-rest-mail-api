7a41d2159bd3eedf666e62f1146ee64686feea67
Korges <korgesik@gmail.com>
2020-07-05 10:26:12 +0200

    Add messages and codes to Error enum.

--- src/main/java/com/korges/demo/model/enums/Error.java
+++ src/main/java/com/korges/demo/model/enums/Error.java
@@ -1,8 +1,17 @@
 package com.korges.demo.model.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Error {
-    NOT_FOUND,
-    SENT,
-    NO_RECIPIENTS,
-    UNSPECIFIED
+    NOT_FOUND("Email not found by given id", 404),
+    SENT("Email has been already sent", 400),
+    NO_RECIPIENTS("Email has no defined recipients", 400),
+    UNSPECIFIED("", 500);
+
+    private final String message;
+    private final int code;
+
 }
