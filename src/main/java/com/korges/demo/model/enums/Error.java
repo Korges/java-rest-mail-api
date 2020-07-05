296c745e54653b38bca70c8843bf349184303b0e
Korges <korgesik@gmail.com>
2020-07-05 13:25:11 +0200

    Update Error enum constants.

--- src/main/java/com/korges/demo/model/enums/Error.java
+++ src/main/java/com/korges/demo/model/enums/Error.java
@@ -2,16 +2,19 @@ package com.korges.demo.model.enums;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
 
 @Getter
 @RequiredArgsConstructor
 public enum Error {
-    NOT_FOUND("Email not found by given id", 404),
-    SENT("Email has been already sent", 400),
-    NO_RECIPIENTS("Email has no defined recipients", 400),
-    UNSPECIFIED("", 500);
+    NOT_FOUND("Email not found by given id", HttpStatus.NOT_FOUND),
+    SENT("Email has been already sent", HttpStatus.BAD_REQUEST),
+    NO_RECIPIENTS("Email has no defined recipients", HttpStatus.BAD_REQUEST),
+    MAIL_SENDER_ERROR("MailSenderService Exception. Please check logs.", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNSPECIFIED("", HttpStatus.NOT_IMPLEMENTED);
 
     private final String message;
-    private final int code;
+    private final HttpStatus code;
 
 }
